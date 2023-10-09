af96e6eb2a12b2407f24ac0373ce2d63e746aca2
Amirhossein Moznebi <59997964+amirimoo@users.noreply.github.com>
2023-10-09 23:12:24 +0330

    Add "add" Function

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# start define the functions needed : add , sub , mul , div
+#print option to the user
+#ask for values  
+#call the funnctions 
+# while loop to continue the program 
+
+# start add Function 
+def add(a,b):
+    answer = a+b 
+    print(str(a) + " + " + str(b) + " = " + str(answer) )
+# finish add function 
